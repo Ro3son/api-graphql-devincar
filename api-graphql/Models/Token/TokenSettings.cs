ebd0059aedd6bf8fbe6415651b483807325ca374
Robson De Andrade <theairelibre@protonmail.com>
2022-10-16 15:19:06 -0300

    Nullable

--- api-graphql/Models/Token/TokenSettings.cs
+++ api-graphql/Models/Token/TokenSettings.cs
@@ -2,8 +2,8 @@ namespace DevInCar.GraphQL.Models
 {
     public class TokenSettings
     {
-        public string Issuer { get; set; }
-        public string Audience { get; set; }
-        public string Key { get; set; }
+        public string? Issuer { get; set; }
+        public string? Audience { get; set; }
+        public string? Key { get; set; }
     }
 }

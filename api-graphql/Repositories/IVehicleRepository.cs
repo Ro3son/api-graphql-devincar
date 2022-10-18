95a85c7efca1be4a664b66e0b12771992175ccf7
Robson De Andrade <theairelibre@protonmail.com>
2022-10-18 15:19:13 -0300

    Add method AddVehicle

--- api-graphql/Repositories/IVehicleRepository.cs
+++ api-graphql/Repositories/IVehicleRepository.cs
@@ -9,6 +9,7 @@ namespace DevInCar.GraphQL.Repositories
         IEnumerable<Veiculo> GetByStatusVehicle(EStatusVeiculo statusVeiculo);
         IEnumerable<Veiculo> GetMaxPrice();
         IEnumerable<Veiculo> GetMinPrice();
-        Veiculo SellVehicle(int veiculoId);
+        Veiculo SellVehicle(int veiculoId, string cpf);
+        void AddVehicle(Veiculo veiculo);
     }
 }

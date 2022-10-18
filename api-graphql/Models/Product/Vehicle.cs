2c58a984d3293d9626ad31ac81bb17c33d48acb1
Robson De Andrade <theairelibre@protonmail.com>
2022-10-18 11:06:40 -0300

    Implement Interface

--- api-graphql/Models/Product/Vehicle.cs
+++ api-graphql/Models/Product/Vehicle.cs
@@ -1,11 +1,13 @@
 namespace DevInCar.GraphQL.Models
 {
-    public class Veiculo : VeiculoModel
+    public class Veiculo : VeiculoModel, IVenda
     {
         public int VeiculoId { get; set; }
         public double Potencia { get; set; }
         public string? Combustivel { get; set; }
         public int Portas { get; set; }
         public int Rodas { get; set; }
+        public int VendaId { get; set; }
+        public string? CPF { get; set; }
     }
 }

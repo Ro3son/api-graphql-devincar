5209f003c4bea36a4f2dbd45a722422479325d25
Robson De Andrade <theairelibre@protonmail.com>
2022-10-18 15:18:25 -0300

    Remove properties and interface

--- api-graphql/Models/Product/Vehicle.cs
+++ api-graphql/Models/Product/Vehicle.cs
@@ -1,13 +1,11 @@
 namespace DevInCar.GraphQL.Models
 {
-    public class Veiculo : VeiculoModel, IVenda
+    public class Veiculo : VeiculoModel
     {
         public int VeiculoId { get; set; }
         public double Potencia { get; set; }
         public string? Combustivel { get; set; }
         public int Portas { get; set; }
         public int Rodas { get; set; }
-        public int VendaId { get; set; }
-        public string? CPF { get; set; }
     }
 }

568366e09724ca7f3514b16d32ae47fc0fd07592
Robson De Andrade <theairelibre@protonmail.com>
2022-10-18 15:17:28 -0300

    Create Method AddVehicle

--- api-graphql/GraphQL/Mutations/VehiclesMutation.cs
+++ api-graphql/GraphQL/Mutations/VehiclesMutation.cs
@@ -8,7 +8,29 @@ namespace DevInCar.GraphQL.Mutations
     {
         [GraphQLName("vender_veiculo")]
         [GraphQLDescription("Método para vender veículo.")]
-        public Veiculo SellVehicle(int id, [Service] IVehicleRepository repository) =>
-            repository.SellVehicle(id);
+        public Veiculo SellVehicle(int id, string cpf, [Service] IVehicleRepository _repository) =>
+            _repository.SellVehicle(id, cpf);
+
+        [GraphQLName("post_veiculo")]
+        [GraphQLDescription("Método para criar novo veículo.")]
+        public Veiculo PostVeiculo(
+            int veiculoId,
+            string chassi,
+            string placa,
+            string nomeModelo,
+            
+            [Service] IVehicleRepository _repository
+        )
+        {
+            Veiculo veiculo = new Veiculo();
+            veiculo.VeiculoId = veiculoId;
+            veiculo.Chassi = chassi;
+            veiculo.Placa = placa;
+            veiculo.NomeModelo = nomeModelo;
+
+            _repository.AddVehicle(veiculo);
+
+            return veiculo;
+        }
     }
 }

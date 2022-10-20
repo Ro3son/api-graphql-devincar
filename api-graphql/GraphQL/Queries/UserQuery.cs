c77db2742983366afff4788195daa60d030fdb64
Robson De Andrade <theairelibre@protonmail.com>
2022-10-19 23:07:36 -0300

    Add Queries

--- api-graphql/GraphQL/Queries/UserQuery.cs
+++ api-graphql/GraphQL/Queries/UserQuery.cs
@@ -1,11 +1,21 @@
 using DevInCar.GraphQL.Models;
 using DevInCar.GraphQL.Repositories;
+using HotChocolate.AspNetCore.Authorization;
 
 namespace DevInCar.GraphQL.Queries
 {
     [ExtendObjectType(OperationTypeNames.Query)]
-    public class UserQuery
+    public class UsuariosQuery
     {
-        
+        [Authorize]
+        [GraphQLName("buscar_usuarios")]
+        [GraphQLDescription("Método para retornar todos os usuários cadastrados.")]
+        public IEnumerable<Usuario> Get([Service] IUserRepository repository) =>
+            repository.GetUsers();
+
+        [GraphQLName("buscar_usuario_por_id")]
+        [GraphQLDescription("Método para retornar o usuário por id.")]
+        public Usuario GetUser([Service] IUserRepository repository, int userID) =>
+            repository.GetUser(userID);
     }
 }

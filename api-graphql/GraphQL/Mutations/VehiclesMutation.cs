81e418de16392056337c8f51d22d4d1e479a9b18
Robson De Andrade <theairelibre@protonmail.com>
2022-10-20 14:56:27 -0300

    Modify File

--- api-graphql/GraphQL/Mutations/VehiclesMutation.cs
+++ api-graphql/GraphQL/Mutations/VehiclesMutation.cs
@@ -1,5 +1,7 @@
 using DevInCar.GraphQL.Models;
 using DevInCar.GraphQL.Repositories;
+using DevInCar.GraphQL.Subscriptions;
+using HotChocolate.Subscriptions;
 
 namespace DevInCar.GraphQL.Mutations
 {
@@ -9,52 +11,92 @@ namespace DevInCar.GraphQL.Mutations
         [GraphQLName("vender_veiculo")]
         [GraphQLDescription("Método para vender veículo.")]
         public Veiculo SellVehicle(
-            int id, 
-            string cpf, 
-            [Service] IVehicleRepository _repository) =>
-            _repository.SellVehicle(id, cpf);
+            int veiculoId, string cpf, 
+            [Service] IVehicleRepository _repository,
+            [Service] ITopicEventSender _eventSender)
+        {
+            try 
+            { 
+                var veiculo = _repository.SellVehicle(veiculoId, cpf);
+
+                _eventSender
+                    .SendAsync(nameof(VeiculosSubscription.VeiculoAdicionado), veiculo)
+                    .ConfigureAwait(false);
+                
+                return veiculo;
+            }
+            catch (System.Exception)
+            {
+                throw new Exception("Não foi possível vender o veículo!");
+            }
+        }
 
         [GraphQLName("criar_veiculo")]
         [GraphQLDescription("Método para criar um novo veículo.")]
-        public bool PostVeiculo(
+        public Veiculo PostVeiculo(
             VeiculoInput input,
-            [Service] IVehicleRepository _repository
+            [Service] IVehicleRepository _repository,
+            [Service] ITopicEventSender _eventSender
         )
         {
-            Veiculo veiculo = new Veiculo()
+            try
             {
-                VeiculoId = input.VeiculoId,
-                Chassi = input.Chassi,
-                Placa = input.Placa,
-                NomeModelo = input.NomeModelo
-            };
+                Veiculo veiculo = new Veiculo();
+                veiculo.VeiculoId = input.VeiculoId;
+                veiculo.Chassi = input.Chassi;
+                veiculo.Placa = input.Placa;
+                veiculo.NomeModelo = input.NomeModelo;
 
-            return _repository.AddVehicle(veiculo);
+                _repository.AddVehicle(veiculo);
 
+                _eventSender
+                    .SendAsync(nameof(VeiculosSubscription.VeiculoAdicionado), veiculo)
+                    .ConfigureAwait(false);
+
+                return veiculo;
+            }
+            catch (System.Exception)
+            {
+                throw new Exception("Não foi possível criar o veículo!");
+            }
         }
 
         [GraphQLName("alterar_cor")]
         [GraphQLDescription("Método para alterar a cor de um veículo.")]
         public bool PutColor(int veiculoId, string cor, [Service] IVehicleRepository _repository)
         {
-            var model = new Veiculo();
-            model.VeiculoId = veiculoId;
-            model.Cor = cor;
+            try
+            {
+                var model = new Veiculo();
+                model.VeiculoId = veiculoId;
+                model.Cor = cor;
 
-            return _repository.UpdateColor(veiculoId, model);
+                return _repository.UpdateColor(veiculoId, model);
+
+            }
+            catch(System.Exception)
+            {
+                throw new Exception("Não foi possível alterar a cor!");
+            }
         }
 
         [GraphQLName("alterar_valor")]
         [GraphQLDescription("Método para alterar o valor de um veículo.")]
         public bool PutValue(int veiculoId, decimal preco, [Service] IVehicleRepository _repository)
         {
-            var model = new Veiculo 
-            { 
-                VeiculoId = veiculoId, 
-                Preco = preco 
-            };
+            try
+            {
+                var model = new Veiculo();
+                model.VeiculoId = veiculoId;
+                model.Preco = preco;
 
-            return _repository.UpdateColor(veiculoId, model);
+                return _repository.UpdatePrice(veiculoId, model);
+
+            }
+            catch(System.Exception)
+            {
+               throw new Exception("Não foi possível alterar o valor!");
+            }
         }
     }
 }
